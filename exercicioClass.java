2fe7c01b58722469f183d28c3c43fc8ab14031bd
lukas <lukasrar001@gmail.com>
2018-08-07 15:54:54 -0300

    att

--- exercicioClass.java
+++ exercicioClass.java
@@ -8,18 +8,20 @@ public class exercicioClass {
 		String nome[] = new String[n];
 		String nomeSub[] = new String[n];
 		int nomeSubInt[] = new int[n];
-		int maiorNota = 99999999;
+		int maiorNota = 0;
+		String maiorNotaAluno = "";
+		int posNome = 0;
 		for (int i = 0; i < n; i++) {
 			nome[i] = sc.next();
-			nomeSub[i] = nome[i].substring(nome[i].length() - 2);
+			nomeSub[i] = nome[i].substring(nome[i].length()-2);
 			nomeSubInt[i] = Integer.parseInt(nomeSub[i]);
-			if (nomeSubInt[i] < maiorNota) {
-				System.out.println(nomeSubInt[i]);
-			}else {
-				System.out.println(nomeSubInt[i]);
+			if (nomeSubInt[i] > maiorNota) {
+				maiorNotaAluno = nome[i].substring(0, nome[i].length()-18);
+				maiorNota = nomeSubInt[i];
+				posNome = i;
 			}
-			
 		}
+		System.out.println("O Aluno "+maiorNotaAluno+" tirou a nota igual a "+maiorNota);
 	}
 
 }
